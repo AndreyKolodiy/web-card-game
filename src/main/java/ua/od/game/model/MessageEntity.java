d3f40de46cce5dc53b6bb3a8959fba5b9acf1013
mr.andreykolodiy@gmail.com <mr.andreykolodiy@gmail.com>
2019-01-08 13:20:29 +0500

    MessegDaoImpl creat method getMessegeList

--- src/main/java/ua/od/game/model/MessageEntity.java
+++ src/main/java/ua/od/game/model/MessageEntity.java
@@ -8,4 +8,21 @@ public class MessageEntity {
     private Integer fromAccountId;
     private Integer toAccountId;
     private Date time;
+
+
+    public void setFromAccountId(Integer fromAccountId) {
+        this.fromAccountId = fromAccountId;
+    }
+    public void setToAccountId(Integer toAccountId) {
+        this.toAccountId = toAccountId;
+    }
+    public void setTime (Date time){
+        this.time = time;
+    }
+    public void setText (String text){
+        this.text = text;
+    }
+    public void setId (Integer id){
+        this.id = id;
+    }
 }
--- src/main/java/ua/od/game/repository/dao/impl/MessageDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/MessageDaoImpl.java
@@ -4,23 +4,35 @@ import ua.od.game.model.MessageEntity;
 import ua.od.game.repository.dao.MessageDao;
 import ua.od.game.repository.helper.SqlHelper;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Collections;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 public class MessageDaoImpl implements MessageDao {
 
     @Override
     public List<MessageEntity> getMessageList(Integer fromAccountId, Integer toAccountId, Date fromTime) {
-       return null;
+        MessageEntity message = SqlHelper.prepareStatement("SELECT * FROM Message WHERE from_account_id = ? AND to_account_id = ? AND time = ?", pstmt -> {
+            pstmt.setInt(1, fromAccountId);
+            pstmt.setInt(2, toAccountId);
+            pstmt.setDate(3, (java.sql.Date) fromTime);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next() ? new MessageEntity() {{
+                setFromAccountId(rs.getInt("from_account_id"));
+                setToAccountId(rs.getInt("to_account_id"));
+                setTime(rs.getDate("time"));
+                setText(rs.getString("text"));
+            }} : null;
+        });
+
+        return Collections.singletonList(message);
     }
+
 
     @Override
     public Boolean sendMessage(MessageEntity message) {
         return null;
     }
 }
+
