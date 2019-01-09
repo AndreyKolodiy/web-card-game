2f6dc24434c6a85f29a730e3b92d48305ed113ca
mr.andreykolodiy@gmail.com <mr.andreykolodiy@gmail.com>
2019-01-09 19:43:06 +0500

    Update the MessageDaoImpl

--- src/main/java/ua/od/game/repository/dao/impl/MessageDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/MessageDaoImpl.java
@@ -7,26 +7,31 @@ import ua.od.game.repository.helper.SqlHelper;
 import java.sql.ResultSet;
 import java.util.Collections;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MessageDaoImpl implements MessageDao {
 
+    private final String GET_MessageList = "SELECT * FROM Message WHERE from_account_id = ? AND to_account_id = ? AND time = ?";
+
     @Override
     public List<MessageEntity> getMessageList(Integer fromAccountId, Integer toAccountId, Date fromTime) {
-        MessageEntity message = SqlHelper.prepareStatement("SELECT * FROM Message WHERE from_account_id = ? AND to_account_id = ? AND time = ?", pstmt -> {
+        return SqlHelper.prepareStatement(GET_MessageList, pstmt -> {
             pstmt.setInt(1, fromAccountId);
             pstmt.setInt(2, toAccountId);
-            pstmt.setDate(3, (java.sql.Date) fromTime);
+            pstmt.setDate(3, new java.sql.Date(fromTime.getTime()));
             ResultSet rs = pstmt.executeQuery();
-            return rs.next() ? new MessageEntity() {{
-                setFromAccountId(rs.getInt("from_account_id"));
-                setToAccountId(rs.getInt("to_account_id"));
-                setTime(rs.getDate("time"));
-                setText(rs.getString("text"));
-            }} : null;
+            List<MessageEntity> message = new LinkedList<>();
+            while (rs.next()) {
+                message.add(new MessageEntity() {{
+                    setFromAccountId(rs.getInt("from_account_id"));
+                    setToAccountId(rs.getInt("to_account_id"));
+                    setTime(rs.getDate("time"));
+                    setText(rs.getString("text"));
+                }});
+            }
+            return message;
         });
-
-        return Collections.singletonList(message);
     }
 
 
