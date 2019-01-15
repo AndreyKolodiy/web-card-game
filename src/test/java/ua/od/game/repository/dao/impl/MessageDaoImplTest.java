cf91bc371af2894b2c7fb028c5ec194bd1c849ab
mr.andreykolodiy@gmail.com <mr.andreykolodiy@gmail.com>
2019-01-15 15:50:24 +0500

    Cheng

--- src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
@@ -1,7 +1,10 @@
 package ua.od.game.repository.dao.impl;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +21,10 @@ public class MessageDaoImplTest {
 
     @Test
     public void getMessageList() {
+
+        Assert.assertTrue(!messageDao.getMessageList(111,222,new Date()).isEmpty());
+        Assert.assertTrue(!messageDao.getMessageList(111,222,new Date()).isEmpty());
+        Assert.assertTrue(!messageDao.getMessageList(222,111,new Date()).isEmpty());
     }
 
     @Test
