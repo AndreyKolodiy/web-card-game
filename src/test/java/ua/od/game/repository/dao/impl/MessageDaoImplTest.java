d780a4d8ff240cb056739636a919fc60adee0baf
mr.andreykolodiy@gmail.com <mr.andreykolodiy@gmail.com>
2019-01-16 22:31:37 +0500

    MessageDaoTest

--- src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
@@ -3,8 +3,12 @@ package ua.od.game.repository.dao.impl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import sun.util.calendar.BaseCalendar;
+import ua.od.game.model.MessageEntity;
+import ua.od.game.repository.dao.MessageDao;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,10 +25,14 @@ public class MessageDaoImplTest {
 
     @Test
     public void getMessageList() {
+        List<MessageEntity> message = messageDao.getMessageList(111,222,new Date(2019-01-15-10-59-00));
+        for (int i = 0; i < message.size(); i++) {
+            System.out.print(message.get(i).getFromAccountId() + " ");
+            System.out.print(message.get(i).getToAccountId() + " ");
+            System.out.print(message.get(i).getText()+ " ");
+            System.out.print(message.get(i).getTime()+ " ");
+        }
 
-        Assert.assertTrue(!messageDao.getMessageList(111,222,new Date()).isEmpty());
-        Assert.assertTrue(!messageDao.getMessageList(111,222,new Date()).isEmpty());
-        Assert.assertTrue(!messageDao.getMessageList(222,111,new Date()).isEmpty());
     }
 
     @Test
