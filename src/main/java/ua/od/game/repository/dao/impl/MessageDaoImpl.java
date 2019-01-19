a112b525b74ff24abe5312932a1aa47ec4bd1423
mr.andreykolodiy@gmail.com <mr.andreykolodiy@gmail.com>
2019-01-19 21:32:38 +0500

    modified MessageDaoImpl and MessageDaoImplTest and add 10_Messages.sql

--- src/main/java/ua/od/game/repository/dao/impl/MessageDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/MessageDaoImpl.java
@@ -6,6 +6,7 @@ import ua.od.game.repository.dao.MessageDao;
 import ua.od.game.repository.helper.SqlHelper;
 
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,14 +23,14 @@ public class MessageDaoImpl implements MessageDao {
         return SqlHelper.prepareStatement(GET_MESSAGES, pstmt -> {
             pstmt.setInt(1, fromAccountId);
             pstmt.setInt(2, toAccountId);
-            pstmt.setDate(3, new java.sql.Date(fromTime.getTime()));
+            pstmt.setTimestamp(3, new Timestamp(fromTime.getTime()));
             ResultSet rs = pstmt.executeQuery();
             List<MessageEntity> message = new LinkedList<>();
             while (rs.next()) {
                 message.add(new MessageEntity() {{
                     setFromAccountId(rs.getInt("from_account_id"));
                     setToAccountId(rs.getInt("to_account_id"));
-                    setTime(rs.getDate("time"));
+                    setTime(rs.getTimestamp("time"));
                     setText(rs.getString("text"));
                 }});
             }
--- src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
@@ -1,38 +1,41 @@
 package ua.od.game.repository.dao.impl;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import sun.util.calendar.BaseCalendar;
 import ua.od.game.model.MessageEntity;
-import ua.od.game.repository.dao.MessageDao;
+import ua.od.game.repository.dao.DbTest;
 
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * @author Andrey Kolodiy
  */
 
-public class MessageDaoImplTest {
+public class MessageDaoImplTest extends DbTest {
     MessageDaoImpl messageDao;
 
     @Before
-    public void init(){
-        messageDao = new MessageDaoImpl();}
+    public void init() {
+        messageDao = new MessageDaoImpl();
+    }
 
     @Test
-    public void getMessageList() {
-        List<MessageEntity> message = messageDao.getMessageList(111,222,new Date(2019-01-15-10-59-00));
+    public void getMessageListTest() {
+        String dateText = "2019-01-15 10:45:00";
+        Date dateTime = Timestamp.valueOf(dateText);
+        Date CounterDateTame = null;
+        List<MessageEntity> message = messageDao.getMessageList(222, 111, dateTime);
         for (int i = 0; i < message.size(); i++) {
-            System.out.print(message.get(i).getFromAccountId() + " ");
-            System.out.print(message.get(i).getToAccountId() + " ");
-            System.out.print(message.get(i).getText()+ " ");
-            System.out.print(message.get(i).getTime()+ " ");
+            System.out.print("Fron account: " + message.get(i).getFromAccountId() + " ");
+            System.out.println("To account: " + message.get(i).getToAccountId() + " ");
+            System.out.println("Text  Message: " + message.get(i).getText() + " ");
+            System.out.println("Date and time: " + message.get(i).getTime() + " ");
+            System.out.println(" ");
+            CounterDateTame = message.get(i).getTime();
         }
-
+        assert CounterDateTame.after(dateTime);
     }
 
     @Test
